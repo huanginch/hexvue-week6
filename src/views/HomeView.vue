d74931b024e6de2dee20260d9633b90466b7b92c
huanginch <huanginch329@gmail.com>
2023-02-16 21:21:08 +0800

    Add getCart() and getAuth() to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,3 +3,21 @@
     <h1>這裡是首頁(･∀･)</h1>
   </div>
 </template>
+
+<script>
+import cartStore from "../stores/cartStore.js";
+import authStore from "../stores/authStore.js";
+import { mapActions } from "pinia";
+
+export default {
+  name: "HomeView",
+  methods: {
+    ...mapActions(cartStore, ["getCart"]),
+    ...mapActions(authStore, ["getAuth"]),
+  },
+  mounted() {
+    this.getCart();
+    this.getAuth();
+  },
+};
+</script>
